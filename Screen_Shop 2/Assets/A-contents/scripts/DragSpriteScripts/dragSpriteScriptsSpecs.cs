5a254a8c1b392ba83ee12e6348aaa8438e098406
mandeepdhalor28 <apple@Apples-MacBook-Pro.local>
2015-10-13 20:19:51 +0530

    13th October 2015
    Development Build : 13th October 2015

---
+++ Screen_Shop 2/Assets/A-contents/scripts/DragSpriteScripts/dragSpriteScriptsSpecs.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class dragSpriteScriptsSpecs : MonoBehaviour{
+
+	private Vector3 dist;
+
+	public GameObject draggableObject;
+
+	public Vector3 startPos;
+
+	public Vector3 currentPos;
+
+	public float xPos;
+
+	public float yPos;
+
+	public bool startMouseRefresh;
+
+	void Start () {
+
+		startMouseRefresh = false;
+
+	}
+	
+
+	void Update () {
+
+		if(Input.GetMouseButton(0)){
+
+			RaycastHit hit;
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit,1000.0f)){
+
+				if(hit.transform.tag == "Specs"){
+
+
+					currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+					print (xPos + "||"+ yPos);
+
+					if(currentPos.z!=-9){
+
+						currentPos.z= -9;
+
+					}
+				//this.GetComponent<Collider>().transform.position = currentPos;
+
+					OnDragScript();
+
+					}
+
+				}
+			}
+		}
+
+
+
+	public void OnDragScript(){
+
+		//print ("dragging");
+
+//		currentPos = new Vector3(xPos,yPos,0);
+
+		draggableObject.transform.position = currentPos;
+
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+
+		//print ("head is collided");
+
+		}
+
+}
